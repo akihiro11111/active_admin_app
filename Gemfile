c5901b7a623054eeba05f2e30b287065dc826317
inaaki <ina1111koolboost@gmail.com>
2019-08-25 07:48:11 +0900

    add Chart.js

--- Gemfile
+++ Gemfile
@@ -59,3 +59,7 @@ gem 'ransack'
 gem 'redcarpet', '~> 2.3.0'
 #シンタックスハイライト
 gem 'coderay'
+#chartグラフ
+gem 'chart-js-rails', '~> 0.1.4'
+#データの受け渡し
+gem 'gon'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def index
     user = User.new(params_user_search)
     @users = user.search
+    gon.labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orenge"]
+    gon.scores = [1, 2, 3, 4, 5, 6]
   end
 
   private
